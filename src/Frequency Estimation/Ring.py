663bfb7474a223c7f4bec93ae6fcae6444b8b24b
hexingyu <244507302@qq.com>
2022-02-28 15:15:37 +0800

    增添环机制的基础结构

---
+++ src/Frequency Estimation/Ring.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/2/28 3:15 PM
+# @Author : 贺星宇
+# @File : Ring.py
+# @Software: PyCharm
+import numpy as np
+from random import choice
+from collections import Counter
+
+class HM_USER(object):
+    def __init__(self):
+        pass
+
+class HM_SERVER(object):
+    def __init__(self):
+        pass
